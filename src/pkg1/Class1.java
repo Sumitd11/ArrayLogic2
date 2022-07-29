18e36b84ff485ca086b23f34a3352899b34c379f
unknown <sumitdongare1998@gmail.com>
2022-07-29 19:55:59 +0530

    variable names changed

--- src/pkg1/Class1.java
+++ src/pkg1/Class1.java
@@ -7,23 +7,27 @@ public class Class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int []a = {10,20,30,40,50};
+		int [] c = {10,20,30,40,50};
 		
 		//a[0]=10;
+		
 		//a[1]=20;
+		
 		//a[2]=30;
+		
 		//a[3]=40;
+		
 		//a[4]=50;
 		
-		int b=a.length;	
-		System.out.println(Arrays.toString(a));
+		int d=c.length;	
+		System.out.println(Arrays.toString(c));
 		//Replace last and First Element
-		int p=a[0]; //temporary variable
-		a[0]=a[b-1];
-		System.out.println(Arrays.toString(a));
+		int p=c[0]; //temporary variable
+		c[0]=c[d-1];
+		System.out.println(Arrays.toString(c));
 		
-		a[b-1]=p;
-	System.out.println(Arrays.toString(a));
+		c[d-1]=p;
+	System.out.println(Arrays.toString(c));
 
 	}
 
