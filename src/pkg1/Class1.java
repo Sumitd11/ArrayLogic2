c5e705ade522e15cf08eb36f3a1cf57dca94d99b
Sumitd11 <110182378+Sumitd11@users.noreply.github.com>
2022-07-29 20:14:29 +0530

    Update Class1.java

--- src/pkg1/Class1.java
+++ src/pkg1/Class1.java
@@ -7,7 +7,7 @@ public class Class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int [] c = {10,20,30,40,50};
+		int [] x = {10,20,30,40,50};
 		
 		//a[0]=10;
 		
@@ -19,15 +19,15 @@ public class Class1 {
 		
 		//a[4]=50;
 		
-		int d=c.length;	
-		System.out.println(Arrays.toString(c));
+		int y=x.length;	
+		System.out.println(Arrays.toString(x));
 		//Replace last and First Element
-		int p=c[0]; //temporary variable
-		c[0]=c[d-1];
-		System.out.println(Arrays.toString(c));
+		int z=x[0]; //temporary variable
+		x[0]=x[y-1];
+		System.out.println(Arrays.toString(x));
 		
-		c[d-1]=p;
-	System.out.println(Arrays.toString(c));
+		x[d-1]=z;
+	System.out.println(Arrays.toString(x));
 
 	}
 
